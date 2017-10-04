587cd894b0f6a5e22891648586304cea5264c56f
Tobiasz Jarosiewicz <tjarosiewicz@pc2cis.ipj.gov.pl>
2017-10-04 12:12:37 +0200

    Split main into 2 functions of gen_img and show_img.

--- gen_rnd_img.py
+++ gen_rnd_img.py
@@ -5,26 +5,39 @@ Tobiasz Jarosiewicz
 import numpy as np
 import random
 from PIL import Image
+import matplotlib.pyplot as plt
 
-# Give the matrix/array dimmensions
-w, h = 512, 512
-# Fill the matrix with zeros
-data = np.zeros((h, w, 3), dtype=np.uint8)
+def gen_rnd_ar(w, h):
+    # Give the matrix/array dimmensions
+    #w, h = 512, 512
 
-# Check dimmensions:
-print(data.ndim)
-# check data size (w*h*3) because every element has RGB val
-print(data.size)
+    # Fill the matrix with zeros
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    
+    # Check dimmensions:
+    print(data.ndim)
+    # check data size (w*h*3) because every element has RGB val
+    print(data.size)
+    
+    # Fill the matrix with random color values
+    for i in range(0, w, int(w/50)):
+        for j in range(0, h, int(h/50)):
+            r = random.randint(0, 50)
+            g = random.randint(0, 100)
+            b = random.randint(0, 255)
+            data[i][j] = [r, g, b]
+    return data
 
-# Fill the matrix with random color values
-for i in range(0, w):
-    for j in range(0, h):
-        r = random.randint(0, 50)
-        g = random.randint(0, 100)
-        b = random.randint(0, 255)
-        data[i][j] = [r, g, b]
+def show_img(data):
+    plt.imshow(data)
+    plt.title("random")
+    plt.show(block=False)
+    plt.pause(1e-3)
 
-img = Image.fromarray(data, 'RGB')
-# Uncomment to save the image to a file.
-#img.save('asd.png')
-img.show()
+
+img1 = gen_rnd_ar(128, 128)
+show_img(img1)
+
+for i in range(10):
+    img1 = gen_rnd_ar(128, 128)
+    show_img(img1)
